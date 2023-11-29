429dcad0dec02b6672440bc017b305f0d7cee9d1
Michael Hermann <mhermann244@outlook.com>
2023-11-29 11:55:41 +1030

    Update to styling

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavHeaderComponent } from './components/nav-header/nav-header.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { MaterialModule } from './material.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [NavHeaderComponent],
+  declarations: [NavHeaderComponent, FooterComponent],
   imports: [CommonModule, MaterialModule, RouterModule],
-  exports: [MaterialModule, NavHeaderComponent],
+  exports: [MaterialModule, NavHeaderComponent, FooterComponent],
 })
 export class SharedModule {}
